769410e02a9e63e3afcf1f65b518180a03e1f728
Lorenzo McDaniel <39344145+lorenzoMcD@users.noreply.github.com>
2019-04-14 08:57:32 -0500

    Add files via upload

---
+++ McDaniel_PA6/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace McDaniel_PA6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(50, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1069, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 29);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(138, 30);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBox1, "enter state abrv");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(42, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 74);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(542, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(219, 120);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(42, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 75);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(42, 419);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 75);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 7;
+            this.toolTip2.SetToolTip(this.textBox2, "enter shape description");
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 443);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(72, 26);
+            this.textBox3.TabIndex = 8;
+            this.toolTip3.SetToolTip(this.textBox3, "enter month");
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(542, 200);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(494, 142);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(531, 403);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(494, 181);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(381, 443);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 26);
+            this.textBox4.TabIndex = 11;
+            this.toolTip4.SetToolTip(this.textBox4, "enter year");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(374, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Enter in state abrv to search for number of sightings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(428, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Enter in Shape description to find avg duration of the shape";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(631, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Enter in month and year to display datetime,state,country and shape of all UFO si" +
+    "ghtings";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipTitle = "enter state abrv";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 417);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "month";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 417);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "year";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(277, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "shape description";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(281, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "state abrv";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 596);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PA6";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.ToolTip toolTip4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ McDaniel_PA6/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace McDaniel_PA6
+{
+    public partial class Form1 : Form
+    {
+
+        private string filepath;
+        public string line;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            filepath = "";
+
+            label1.Text = "no file selected";
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                filepath = openFileDialog1.FileName;
+                label1.Text = filepath;
+            }
+            else
+            {
+
+                filepath = "";
+                label1.Text = "no file selected";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (filepath == "")
+            {
+
+                MessageBox.Show("Please select file");
+                return;
+            }
+
+
+            string userInput = textBox1.Text;
+            int count = 0;
+
+            using (var sr = new StreamReader(filepath))
+            {
+
+                while (sr.EndOfStream != true)
+                {
+
+                    line = sr.ReadLine();
+
+                    if (line.StartsWith("datetime"))
+                    {
+                        continue;
+                    }
+
+
+                    string[] data = line.Split(',');
+
+                    string state = data[1];
+
+                    if (state == userInput)
+                    {
+                        count++;
+                        richTextBox1.Text = string.Format("Counted {0} UFO sightings in {1}", count, userInput);
+
+                    }
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (filepath == "")
+            {
+
+                MessageBox.Show("Please select file");
+                return;
+            }
+
+
+            string userInput = textBox2.Text;
+          
+            List<int> scores = new List<int>();
+          
+
+            using (var sr = new StreamReader(filepath))
+            {
+              
+                while (sr.EndOfStream != true)
+                {
+
+                    line = sr.ReadLine();
+
+                    if (line.StartsWith("datetime"))
+                    {
+                        continue;
+                    }
+
+
+                    string[] data = line.Split(',');
+
+                    string shape = data[3];
+                    int duration;
+                    bool parseOkay = int.TryParse(data[4], out duration);
+                    
+                    double count = 0;
+                    if (shape == userInput)
+                    {
+                       
+                            count++;
+                            scores.Add(duration);
+                        
+                    }
+
+                }
+
+                double scorecnt = 0;
+                foreach (double val in scores)
+                {
+                   
+                    if (val != 0)
+                    {
+                        scorecnt += val;
+                    }
+                    
+
+                }
+
+
+
+                double avg = Math.Round((double)scorecnt / scores.Count,2);
+                double timecnvrt = Math.Round(avg / 3600,2); // converts to hours 
+                
+                richTextBox2.Text = string.Format(" The average sighting duration for a {0}-shaped UFO is {1} hours ({2} seconds)",userInput, timecnvrt, avg);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (filepath == "")
+            {
+
+                MessageBox.Show("Please select file");
+                return;
+            }
+            
+            int count = 0;
+            string usermonth = textBox3.Text;
+            string useryear = textBox4.Text;
+            using (var sr = new StreamReader(filepath))
+            {
+                if (usermonth == "" || useryear == "")
+                {
+                    MessageBox.Show("must enter data into both fields");
+                }
+
+                while (sr.EndOfStream != true)
+                {
+
+                    line = sr.ReadLine();
+
+                    if (line.StartsWith("datetime"))
+                    {
+                        continue;
+                    }
+
+                    string[] data = line.Split(',', '/',' ');
+
+                   
+
+                    string datayear = data[2];
+                    string datamonth = data[0];
+
+                    string country = data[5];
+                    string shape = data[6];
+                    string state = data[4];
+                    string datetime = string.Format("{0}/{1}/{2} {3}",data[0],data[1],data[2],data[3]);
+                 
+                      if (usermonth == datamonth && useryear == datayear)
+                    {
+                        count++;
+                           
+                        richTextBox3.AppendText(string.Format("{0} {1} {2} {3}, {4}\n ", count, datetime, state, country, shape));
+
+                       }
+                        
+
+                    {
+
+                    }
+             
+
+                }
+
+
+            }
+        }
+    }
+}
+
